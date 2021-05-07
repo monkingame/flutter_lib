3ba04dc18eb0b3e978e51ab6c39d78232ecb3388
sun@notebook-win <65739056@qq.com>
2021-05-07 10:31:22 +0800

    from readable

--- byte_util/test/byte_util_test.dart
+++ byte_util/test/byte_util_test.dart
@@ -1,6 +1,29 @@
 import 'package:byte_util/byte_util.dart';
 
 void main() {
-  // final calculator = Calculator();
-  // print(calculator.addOne(1));
+  testFromReadable();
+  testToReadable();
+  testToBase64();
+  testFromBase64();
+  testClone();
 }
+
+void testFromReadable() {
+  const str1 = '01 02, ff 0x10,0xfa , 90 76 AF a0';
+  final bytes1 = ByteUtil.fromReadable(str1);
+  // [1, 2, 255, 16, 250, 144, 118, 175, 160]
+  print(bytes1);
+
+  const str2 = '101 02 90 01,33 90 76 102, 901';
+  final bytes2 = ByteUtil.fromReadable(str2, radix: Radix.dec);
+  // [101, 2, 90, 1, 33, 90, 76, 102, 133]
+  print(bytes2);
+}
+
+void testToReadable() {}
+
+void testToBase64() {}
+
+void testFromBase64() {}
+
+void testClone() {}
