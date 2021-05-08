348610085e2165d227cb955d8a39486e526078fc
sun@dorm <65739056@qq.com>
2021-05-08 20:32:34 +0800

    byte word

--- modbus_protocal/lib/modbus_protocal.dart
+++ modbus_protocal/lib/modbus_protocal.dart
@@ -4,4 +4,9 @@ library modbus_protocal;
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+
+  void test() {
+    final aaa = 0xff | 0x1;
+    final bbb = aaa << 3;
+  }
 }
