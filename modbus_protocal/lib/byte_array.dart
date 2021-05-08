5b2c11a177dd11dc89d378f72db43b00455394b8
sun@dorm <65739056@qq.com>
2021-05-08 21:23:02 +0800

    combine

--- modbus_protocal/lib/byte_array.dart
+++ modbus_protocal/lib/byte_array.dart
@@ -5,15 +5,36 @@ class ByteArray {
 
   Uint8List get bytes => _bytes;
 
-  ByteArray(List<int> list) {
-    _bytes = Uint8List.fromList(list);
+  ByteArray(List<int> array) {
+    _bytes = Uint8List.fromList(array);
   }
 
   ByteArray.fromByte(int value) {
     _bytes = _toArray(value);
   }
 
+  ByteArray.combineArrays(List<int> array1, List<int> array2) {
+    _bytes = _combine(array1, array2);
+  }
+
   Uint8List _toArray(int value) {
     return Uint8List.fromList([value]);
+  }
+
+  Uint8List _combine(List<int> array1, List<int> array2) {
+    if (array1 == null && array2 == null) return null;
+    if (array1 == null) return Uint8List.fromList(array2);
+    if (array2 == null) return Uint8List.fromList(array1);
+
+    var list = List<int>.filled(array1.length + array2.length, 0);
+
+    for (int i = 0; i < array1.length; i++) {
+      list[i] = array1[i];
+    }
+    for (int i = 0; i < array2.length; i++) {
+      list[array1.length + i] = array2[i];
+    }
+
+    return Uint8List.fromList(list);
   }
 }
--- modbus_protocal/test/modbus_protocal_test.dart
+++ modbus_protocal/test/modbus_protocal_test.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:modbus_protocal/byte_array.dart';
 import 'package:modbus_protocal/byte_word.dart';
 
@@ -23,4 +25,11 @@ void testByteArray() {
 
   final array2 = ByteArray.fromByte(3);
   print(array2.bytes);
+
+  // final bytes = Uint8List.fromList([3, 2, 1]);
+  final array3 = ByteArray(Uint8List.fromList([3, 2, 1]));
+  print(array3.bytes);
+
+  final array4 = ByteArray.combineArrays([1, 2, 3], [0xf0, 0xf2, 0xff]);
+  print(array4.bytes);
 }
