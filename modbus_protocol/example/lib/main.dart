57367d790d0cede32371b43792b88ca38f559b12
sun@notebook-win <65739056@qq.com>
2021-05-10 10:17:28 +0800

    example

--- modbus_protocol/example/lib/main.dart
+++ modbus_protocol/example/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:byte_util/byte_util.dart';
 import 'package:flutter/material.dart';
+import 'package:modbus_protocol/modbus_protocol.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,7 +26,13 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: ElevatedButton(
           child: Text('show crc'),
-          onPressed: () {},
+          onPressed: () {
+            final bytes = [1, 2, 3, 4, 5, 6, 7, 0xff, 0xa1, 0xdd];
+            final modbus = ModbusProtocol(bytes);
+            final crc = modbus.crc;
+            // 0xAF 0x3D
+            print(ByteUtil.toReadable(crc.word));
+          },
         ),
       ),
     );
