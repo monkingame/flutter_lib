090ec71386936227c2da017588c68693c519ee2d
sun@notebook-win <65739056@qq.com>
2021-05-10 16:08:55 +0800

    refactor example

--- image_button/example/lib/main.dart
+++ image_button/example/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image_button/asset_image.dart';
+import 'package:image_button/image_button.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,7 +23,33 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('image button example')),
-      body: Container(),
+      body: Center(
+        child: Column(
+          children: [
+            WidgetAssetImage(assetPath: 'assets/images/bkg_code.png'),
+            WidgetImageButton(
+              imageNormal:
+                  WidgetAssetImage(assetPath: 'assets/images/icon_normal.png'),
+              imageHover:
+                  WidgetAssetImage(assetPath: 'assets/images/icon_hover.png'),
+              onTap: () {
+                print('independent icon image button');
+              },
+            ),
+            WidgetImageButton(
+              imageNormal: WidgetAssetImage(
+                  assetPath: 'assets/images/icons.png',
+                  cropRect: Rect.fromLTWH(61, 168, 92, 92)),
+              imageHover: WidgetAssetImage(
+                  assetPath: 'assets/images/icons.png',
+                  cropRect: Rect.fromLTWH(173, 167, 92, 92)),
+              onTap: () {
+                print('image button icon crop from one image');
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
