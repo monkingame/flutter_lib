959c31411c9ba415ebb08775dedbf48689a12ffd
sun@notebook-win <65739056@qq.com>
2021-05-12 13:50:34 +0800

    refactor

--- byte_util/lib/byte_word.dart
+++ byte_util/lib/byte_word.dart
@@ -1,18 +1,38 @@
 import 'dart:typed_data';
 
+import 'byte.dart';
+
 class ByteWord {
-  final int value;
+  // final int value;
 
-  int _high = 0;
-  int _low = 0;
+  // int _high = 0;
+  // int _low = 0;
 
-  int get high => _high;
-  int get low => _low;
+  // int get high => _high;
+  // int get low => _low;
 
-  Uint8List get word => Uint8List.fromList([_high, _low]);
+  // Uint8List get word => Uint8List.fromList([_high, _low]);
 
-  ByteWord(this.value) {
-    _high = (value >> 8) & 0xFF;
-    _low = value & 0xFF;
+  // ByteWord(this.value) {
+  //   _high = (value >> 8) & 0xFF;
+  //   _low = value & 0xFF;
+  // }
+
+  Byte _high = Byte(0);
+  Byte _low = Byte(0);
+
+  Byte get high => _high;
+  Byte get low => _low;
+
+  ByteWord({required high, required low}) {
+    _high = Byte(high);
+    _low = Byte(low);
   }
+
+  ByteWord.fromInt(int value) {
+    _high = Byte(value >> 8);
+    _low = Byte(value);
+  }
+
+  Uint8List get bytes => Uint8List.fromList([_high.value, _low.value]);
 }
