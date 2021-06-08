603262f6e739d2295e2c9237ac6badfe36576d7e
sun@notebook-win <65739056@qq.com>
2021-06-08 09:49:05 +0800

    refactor

--- dart-now-time-filename/test/dart_now_time_filename_test.dart
+++ dart-now-time-filename/test/dart_now_time_filename_test.dart
@@ -8,27 +8,20 @@ void main() {
     // 20210508-084919-305-149
     print(NowFilename.gen());
 
-    await Future.delayed(Duration(milliseconds: 100));
-
     // header-20210508-084919-314-149
     print(NowFilename.gen(prefix: 'header-'));
-    await Future.delayed(Duration(milliseconds: 100));
 
     // 20210508-084919-314-149.mp3
     print(NowFilename.gen(ext: '.mp3'));
-    await Future.delayed(Duration(milliseconds: 100));
 
     // header-20210508-084919-314-149.mp3
     print(NowFilename.gen(prefix: 'header-', ext: '.mp3'));
-    await Future.delayed(Duration(milliseconds: 100));
 
     // header-20210508@084919@314@149.mp3
     print(NowFilename.gen(prefix: 'header-', ext: '.mp3', seperator: '@'));
-    await Future.delayed(Duration(milliseconds: 100));
 
     // 20210508084919314149.mp3
     print(NowFilename.gen(ext: '.mp3', seperator: null));
-    await Future.delayed(Duration(milliseconds: 100));
 
     print(NowFilename.genSpecifyTime(time: DateTime(1999, 12, 3, 23, 12, 3)));
   });
