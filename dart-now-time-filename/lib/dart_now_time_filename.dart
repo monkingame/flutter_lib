451cecd8d9487060ec639a847bd8f44524a33900
sun@notebook-win <65739056@qq.com>
2021-06-08 09:48:51 +0800

    refactor

--- dart-now-time-filename/lib/dart_now_time_filename.dart
+++ dart-now-time-filename/lib/dart_now_time_filename.dart
@@ -3,9 +3,15 @@ library dart_now_time_filename;
 /// generate now time string for filename.
 class NowFilename {
   /// generate now file name
-  static String gen(
-      {String? prefix: '', String? ext: '', String? seperator: '-'}) {
-    final now = DateTime.now();
+  static String genSpecifyTime({
+    String? prefix: '',
+    String? ext: '',
+    String? seperator: '-',
+    required DateTime? time,
+  }) {
+    // final now = DateTime.now();
+    final now = time == null ? DateTime.now() : time;
+
     final y = now.year.toString().padLeft(4, '0');
     final m = now.month.toString().padLeft(2, '0');
     final d = now.day.toString().padLeft(2, '0');
@@ -15,11 +21,19 @@ class NowFilename {
     final ml = now.millisecond.toString().padLeft(3, '0');
     final mc = now.microsecond.toString().padLeft(3, '0');
 
-    // return '$prefix$y$m$d$seperator$h$mi$s$seperator$ml$seperator$mc$ext';
     final p = prefix ?? '';
     final e = ext ?? '';
     final sp = seperator ?? '';
     return p + y + m + d + sp + h + mi + s + sp + ml + sp + mc + e;
+  }
+
+  static String gen({
+    String? prefix: '',
+    String? ext: '',
+    String? seperator: '-',
+  }) {
+    return genSpecifyTime(
+        prefix: prefix, ext: ext, seperator: seperator, time: DateTime.now());
   }
 
   @Deprecated('Use NowFilename.gen() instead. '
--- dart-now-time-filename/test/dart_now_time_filename_test.dart
+++ dart-now-time-filename/test/dart_now_time_filename_test.dart
@@ -1,22 +1,35 @@
 // import 'package:dart_now_time_filename/dart_now_time_filename.dart';
 
+import 'package:dart_now_time_filename/dart_now_time_filename.dart';
+import 'package:flutter_test/flutter_test.dart';
+
 void main() {
-  // // 20210508-084919-305-149
-  // print(NowFilename.genNowFilename());
+  test('NowFilename', () async {
+    // 20210508-084919-305-149
+    print(NowFilename.gen());
 
-  // // header-20210508-084919-314-149
-  // print(NowFilename.genNowFilename(prefix: 'header-'));
+    await Future.delayed(Duration(milliseconds: 100));
 
-  // // 20210508-084919-314-149.mp3
-  // print(NowFilename.genNowFilename(ext: '.mp3'));
+    // header-20210508-084919-314-149
+    print(NowFilename.gen(prefix: 'header-'));
+    await Future.delayed(Duration(milliseconds: 100));
 
-  // // header-20210508-084919-314-149.mp3
-  // print(NowFilename.genNowFilename(prefix: 'header-', ext: '.mp3'));
+    // 20210508-084919-314-149.mp3
+    print(NowFilename.gen(ext: '.mp3'));
+    await Future.delayed(Duration(milliseconds: 100));
 
-  // // header-20210508@084919@314@149.mp3
-  // print(NowFilename.genNowFilename(
-  //     prefix: 'header-', ext: '.mp3', seperator: '@'));
+    // header-20210508-084919-314-149.mp3
+    print(NowFilename.gen(prefix: 'header-', ext: '.mp3'));
+    await Future.delayed(Duration(milliseconds: 100));
 
-  // // 20210508084919314149.mp3
-  // print(NowFilename.genNowFilename(ext: '.mp3', seperator: null));
+    // header-20210508@084919@314@149.mp3
+    print(NowFilename.gen(prefix: 'header-', ext: '.mp3', seperator: '@'));
+    await Future.delayed(Duration(milliseconds: 100));
+
+    // 20210508084919314149.mp3
+    print(NowFilename.gen(ext: '.mp3', seperator: null));
+    await Future.delayed(Duration(milliseconds: 100));
+
+    print(NowFilename.genSpecifyTime(time: DateTime(1999, 12, 3, 23, 12, 3)));
+  });
 }
