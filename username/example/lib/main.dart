50698618181769a5100125bb83d877c47e13ec7f
sun@notebook-win <65739056@qq.com>
2021-12-17 08:54:20 +0800

    refactor

--- username/example/lib/main.dart
+++ username/example/lib/main.dart
@@ -25,28 +25,19 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     final VoidCallback onTap = () {
-      // Ella Wilson, Daniel Price, Mary James, Charlotte Wright, Hazel Sanchez, Joseph Ali, Aubrey King, Harper Lee, Benjamin Robinson, William Martinez
-      // (actual usernames generated random)
+      // Eli Murphy (actual usernames generated random)
       print(Username.en().fullname);
 
-      final user2 = Username.cn();
-      // 姚燕(实际名称随机产生)
-      print(user2.fullname);
-
-      final user3 = Username.cn();
-      // 严梓宣, 秦浩轩, 苏玉英, 曹徳明, 苏鹏, 邵浩轩, 陈子轩, 顾建国, 严玉兰, 高倩
-      // (实际名称随机产生)
-      print(user3.getFullnames(count: 10));
-
-      final user4 = Username.cn(surName: '王');
-      // 王波, 王国华, 王敏, 王俊杰, 王娟, 王宇, 王宇航, 王英, 王婷婷, 王和平
-      // (实际名称随机产生)
-      print(user4.getFullnames(count: 10));
-
-      final user5 = Username.en();
-      // Scarlett Morris, Micah Doherty, Samuel Sullivan, Makayla Turner, Ethan Miller, Braxton Perez, Nevaeh MacDonald, Natalie Bennett, Kayla Carter, Oakley Reyes
+      // Alexander Robertson, Lincoln Sullivan, Ezekiel Jenkins
       // (actual usernames generated random)
-      print(user5.getFullnames(count: 10));
+      print(Username.en().getFullnames(count: 3));
+
+      // 姚宇 (实际名称随机产生)
+      print(Username.cn().fullname);
+
+      // 李红梅, 李波, 邵玉兰, 史芳, 陶梓涵, 汪超
+      // 姚宇 (实际名称随机产生)
+      print(Username.cn().getFullnames(count: 6));
     };
 
     return Scaffold(
